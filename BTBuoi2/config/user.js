f6202fea34f663f47fd7e0c7386a10aae0e5fd3e
Your Name <tungvip31@gmail.com>
2022-03-26 00:08:04 +0700

    BT_Buoi_2

---
+++ BTBuoi2/config/user.js
@@ -0,0 +1,35 @@
+module.exports ={
+users : [
+ {
+   id: 1,
+   Name: "Nguyen Van A"
+ },
+ {
+   id: 2,
+   Name: "Nguyen Van B"
+ },
+ {
+   id: 3,
+   Name: "Nguyen Van C"
+ },
+],
+
+comments : [
+ {
+   id: 1,
+   user_id:  1,
+   content: "Hello nodejs"
+ },
+ {
+   id: 2,
+   user_id:  3,
+   content: "Hello nodejs JS"
+ },
+ {
+   id: 3,
+   user_id:  2,
+   content: "Hello nodejs JS Hee"
+ },
+]
+
+}
--- BTBuoi2/index.js
+++ BTBuoi2/index.js
@@ -1,1 +1,7 @@
 
+
+var http = require('http');
+
+const config = require('./config/config')
+
+ http.createServer(config.test).listen(config.port,console.log('connected locallhost:', config.port))
